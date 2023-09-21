2c19ad51ea3afa03724c2bf3b45e8f0e88ad6e08
Rakshith-1 <akularakshith@gmail.com>
2023-09-21 19:53:45 +0530

    Jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                // Checkout the source code from the Git repository
+                checkout scm
+            }
+        }
+        
+        stage('Build') {
+            steps {
+                // Compile the Java source code
+                sh 'javac MyApp.java'
+            }
+        }
+
+        stage('Run') {
+            steps {
+                // Run the Java program
+                sh 'java MyApp'
+            }
+        }
+    }
+}
